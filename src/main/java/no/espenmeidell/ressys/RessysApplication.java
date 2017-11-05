ed2d804f7f5bc58aabe962533e64f4672fb05d94
Espen Meidell <espen.meidell@gmail.com>
2017-11-05 22:28:36 +0100

    Initial Commit

---
+++ src/main/java/no/espenmeidell/ressys/RessysApplication.java
@@ -0,0 +1,12 @@
+package no.espenmeidell.ressys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RessysApplication.class, args);
+	}
+}
